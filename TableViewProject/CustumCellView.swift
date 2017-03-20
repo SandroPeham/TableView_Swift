e533b56e2df3737d7b7b778b237a122cc268fc59
Sandro Peham <sandropeham@Sandros-iMac.local>
2017-03-20 23:54:58 +0100

    Removed File

---
+++ TableViewProject/CustumCellView.swift
@@ -0,0 +1,20 @@
+//
+//  CustumCellView.swift
+//  TableViewProject
+//
+//  Created by Sandro Peham on 19/03/2017.
+//  Copyright © 2017 Sandro Peham. All rights reserved.
+//
+
+import Cocoa
+
+class CustumCellView: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+    
+}
